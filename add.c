7ba85abbaa5bb177899bac7f01b379ce4b087dea
Varane1999 <harrowadewale@gmail.com>
2023-12-17 16:38:34 -0800

    edits

--- add.c
+++ add.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
+ * w_add - adds the stack's top two components.
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_add(stack_t **head, unsigned int counter)
+void w_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
